78fb3ad2bd22f48c770792c3c4901c24922d060f
Denis Molodtsov <molodtsovdenis@gmail.com>
2019-06-22 22:06:25 -0400

    inital commit

---
+++ src/extensions/routing/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IRoutingApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'RoutingApplicationCustomizerStrings' {
+  const strings: IRoutingApplicationCustomizerStrings;
+  export = strings;
+}
